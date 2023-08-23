d0c7b8f0b6f557eb8c75359958238257b90c7846
Yakov Damen <yakov.damen@gmail.com>
2023-08-23 16:53:42 +0300

    ts

---
+++ Copy of rockPaperScisoors.ts
@@ -0,0 +1,76 @@
+
+// בנו תכנית שמדמה את המשחק אבן נייר ומספריים
+// התכנית תגדיר מי המנצח לפי חוקיות המשחק
+// אבן מנצח מספריים שמנצחות נייר שמנצח אבן
+
+// דגשים
+// שחקן יכול לבחור בין אבן נייר ומספרים בלבד! אך שחקן יכול שלא תהיה לו בחירה
+// התכנית יכולה להחזיר את המחרוזות
+// 'tie', 'player1', 'player2' 
+// התשובה חייבת להכיל
+// ENUM
+// type/interface
+// union
+// במידה ולשחקן אין בחירה הצג זרקו שגיאה
+// ממשו את הפונקציה הוסיפו טיפוסים לפרמטרים ולערך החזרה של הפונקציה
+
+// ENUM representing the player's choices
+enum Choice {
+    Rock = "rock",
+    Paper = "paper",
+    Scissors = "scissors",
+    None = "none", // Added to represent when the player does not have a choice
+  }
+  
+  // Type representing the possible game outcomes
+  type GameResult = "tie" | "player1" | "player2"  ;
+  
+  // Function to determine the winner of the game
+  function determineWinner(player1: Choice, player2: Choice): GameResult {
+    if (player1 === player2) {
+      return 'tie';
+    }
+  
+    switch (player1) {
+      case Choice.Rock:
+        return player2 === Choice.Scissors ? 'player1' : 'player2';
+      case Choice.Paper:
+        return player2 === Choice.Rock ? 'player1' : 'player2';
+      case Choice.Scissors:
+        return player2 === Choice.Paper ? 'player1' : 'player2';
+      default:
+        throw new Error('Invalid choice for player1');
+    }
+  }
+  
+  // Function to play the game and return the result
+  function playGame(player1: Choice, player2: Choice = Choice.None): GameResult {
+    if (player1 === Choice.None) {
+      throw new Error('Player 1 must make a choice');
+    }
+  
+    if (player2 === Choice.None) {
+      throw new Error('Player 2 must make a choice');
+    }
+  
+    return determineWinner(player1, player2);
+  }
+  
+  // Example of playing the game
+  const player1: Choice = Choice.Rock;
+  const player2: Choice = Choice.Scissors;
+  
+  try {
+    const result: GameResult = playGame(player1, player2);
+    console.log(`Player 1 chose: ${player1}`);
+    console.log(`Player 2 chose: ${player2}`);
+    console.log(`The result is: ${result}`);
+  } catch (error ){
+    console.error(error);
+  }
+  
+
+const play = playGame(player1, player2);
+console.log(play);
+//Output: player1 or player2 or tie
+
